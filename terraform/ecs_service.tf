c208df9af4823bd9714cb9673ea3ad4f6dc1e10a
Tyler Westlie <tyler.westlie@gmail.com>
2026-08-07 13:21:51 -0600

    Enable ECS Exec for production debugging access

--- terraform/ecs_service.tf
+++ terraform/ecs_service.tf
@@ -4,6 +4,7 @@ resource "aws_ecs_service" "app" {
   task_definition = aws_ecs_task_definition.app.arn
   desired_count   = 1
   launch_type     = "FARGATE"
+  enable_execute_command = true
 
   network_configuration {
     subnets          = aws_subnet.private[*].id
