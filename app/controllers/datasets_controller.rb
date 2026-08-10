721f867c7a21b4f7f041b4d5dd2a654c83b2a2a4
Tyler Westlie <32471176+tywestlie@users.noreply.github.com>
2026-08-10 15:58:41 -0600

    adds new views for suprise me and extremes (#35)

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -8,4 +8,36 @@ class DatasetsController < ApplicationController
     @columns = @dataset.dataset_columns.order(:position)
     @rows = @dataset.dataset_rows.order(:id).page(params[:page]).per(50)
   end
+
+  def random
+    @dataset = Dataset.find(params[:id])
+    @columns = @dataset.dataset_columns.order(:position)
+    @row = @dataset.dataset_rows.order(Arel.sql("RANDOM()")).first
+  end
+
+  def extremes
+    @dataset = Dataset.find(params[:id])
+    @columns = @dataset.dataset_columns.order(:position)
+
+    @leaderboards = {
+      "Hottest" => numeric_top(:pl_eqt, :desc),
+      "Coldest" => numeric_top(:pl_eqt, :asc),
+      "Closest to Earth-size" => numeric_top(:pl_rade, :asc, distance_from: 1.0),
+      "Most recently discovered" => numeric_top(:disc_year, :desc),
+      "Closest to Earth" => numeric_top(:sy_dist, :asc)
+    }
+  end
+
+  private
+
+  def numeric_top(field, direction, distance_from: nil)
+    field_expr = ActiveRecord::Base.sanitize_sql_array([ "data->>?", field.to_s ])
+    cast = "(#{field_expr})::float"
+    scope = @dataset.dataset_rows.where(Arel.sql("#{field_expr} IS NOT NULL"))
+
+    order_expr = distance_from ? "ABS(#{cast} - #{distance_from})" : cast
+    order_direction = distance_from ? :asc : direction
+
+    scope.order(Arel.sql("#{order_expr} #{order_direction.to_s.upcase}")).limit(5)
+  end
 end
